49bd405a07c977220c0fe47f28a46a6b0c2d9a74
Bruno Garcia Moura <itsme@bgarciamoura.com>
2024-09-10 19:56:46 -0300

    feat: Included a new theme

---
+++ lua/themes/current-theme.lua
@@ -0,0 +1,10 @@
+require("flow").setup {
+  dark_theme = true,
+  transparent = true,
+  high_contrast = false,
+  fluo_color = "pink",
+  mode = "desaturate",
+  aggressive_spell = false,
+}
+
+vim.cmd("colorscheme flow")
