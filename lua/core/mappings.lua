0aca9a19b974e9da74cc3a5da78a8787d4196a10
Bruno Garcia Moura <itsme@bgarciamoura.com>
2025-02-18 12:35:47 -0300

    feat: Included some QoL mappings

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -39,6 +39,12 @@ map("n", "<C-v>", '"+p', { desc = "Colar no modo normal" })
 map("v", "<C-v>", '"+p', { desc = "Colar no modo visual" })
 map("c", "<C-v>", "<C-r>+", { desc = "Colar no modo comando" })
 
+-- Mapear Ctrl+C para copiar no Neovim (usando o clipboard do sistema)
+map("v", "<leader>c", '"+y', { desc = "Copiar no modo visual" })
+map("n", "<leader>c", '"+y', { desc = "Copiar no modo normal" })
+map("c", "<leader>c", '"+y', { desc = "Copiar no modo comando" })
+map("i", "<leader>c", '"+y', { desc = "Copiar no modo de inserção" })
+
 -- Git Signs
 map("n", "<leader>gb", "<cmd>Gitsigns blame_line<CR>", { desc = "Git Blame na linha atual" })
 map("n", "<leader>gn", "<cmd>Gitsigns next_hunk<CR>", { desc = "Próxima alteração" })
@@ -50,3 +56,55 @@ map("n", "<leader>gr", "<cmd>Gitsigns reset_hunk<CR>", { desc = "Reverter altera
 map("n", "<leader>ss", [[<cmd>lua require("persistence").load()<CR>]], { noremap = true, silent = true })                -- Restaurar última sessão
 map("n", "<leader>ql", [[<cmd>lua require("persistence").load({ last = true })<CR>]], { noremap = true, silent = true }) -- Restaurar sessão mais recente
 map("n", "<leader>sd", [[<cmd>lua require("persistence").stop()<CR>]], { noremap = true, silent = true })                -- Desativar salvamento de sessão na sessão atual
+
+-- Navegar entre buffers
+map("n", "<S-l>", ":bnext<CR>", { desc = "Próximo buffer" })
+map("n", "<S-h>", ":bprevious<CR>", { desc = "Buffer anterior" })
+map("n", "<leader>bd", ":bd<CR>", { desc = "Fechar buffer atual" })
+
+-- Melhor navegação por palavras (evita parar em símbolos)
+map("n", "w", "wzz", { desc = "Mover para a próxima palavra e centralizar" })
+map("n", "b", "bzz", { desc = "Mover para a palavra anterior e centralizar" })
+map("n", "n", "nzz", { desc = "Próxima ocorrência e centralizar" })
+map("n", "N", "Nzz", { desc = "Ocorrência anterior e centralizar" })
+
+-- Rolar a tela sem mover o cursor
+map("n", "<C-u>", "<C-u>zz", { desc = "Scroll para cima e centralizar" })
+map("n", "<C-d>", "<C-d>zz", { desc = "Scroll para baixo e centralizar" })
+
+-- Manter o cursor na posição ao juntar linhas
+map("n", "J", "mzJ`z", { desc = "Juntar linha sem mover cursor" })
+
+-- Adicionar novas linhas sem entrar no modo de inserção
+map("n", "<leader>o", "o<Esc>", { desc = "Nova linha abaixo" })
+map("n", "<leader>O", "O<Esc>", { desc = "Nova linha acima" })
+
+-- Salvar e fechar rapidamente
+map("n", "<leader>w", ":w<CR>", { desc = "Salvar arquivo" })
+map("n", "<leader>x", ":x<CR>", { desc = "Salvar e sair" })
+map("n", "<leader>Q", ":q!<CR>", { desc = "Sair sem salvar" })
+
+-- Movimentação rápida no modo de inserção
+map("i", "<C-h>", "<Left>", { desc = "Mover cursor para a esquerda" })
+map("i", "<C-l>", "<Right>", { desc = "Mover cursor para a direita" })
+map("i", "<C-j>", "<Down>", { desc = "Mover cursor para baixo" })
+map("i", "<C-k>", "<Up>", { desc = "Mover cursor para cima" })
+
+-- Indentar no modo visual e manter seleção
+map("v", "<", "<gv", { desc = "Diminuir indentação" })
+map("v", ">", ">gv", { desc = "Aumentar indentação" })
+
+-- Substituir seleção sem copiar para o clipboard
+map("v", "p", '"_dP', { desc = "Substituir texto sem afetar clipboard" })
+
+-- Abrir Terminal integrado
+map("n", "<leader>t", ":split | terminal<CR>", { desc = "Abrir terminal no split" })
+map("n", "<leader>vt", ":vsplit | terminal<CR>", { desc = "Abrir terminal em vertical" })
+
+-- Alternar entre os dois últimos buffers
+map("n", "<leader><leader>", "<C-^>", { desc = "Alternar entre buffers" })
+
+-- Selecionar e substituir
+map("n", "<leader>sr", ":%s//g<Left><Left>", { desc = "Selecionar e substituir no arquivo" })
+map("v", "<leader>sr", "y:%s/<C-r>0//g<Left><Left>", { desc = "Selecionar e substituir na seleção" })
+map("n", "<leader>sw", ":Spectre<CR>", { desc = "Selecionar e substituir no workspace" })
