9dc1caa6a8731a8de119c8557872f13dd47da094
Bruno Garcia Moura <itsme@bgarciamoura.com>
2025-02-20 10:59:41 -0300

    feat: Included snippets for QoL

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -69,4 +69,15 @@ return {
       vim.g.copilot_tab_fallback = ""
     end,
   },
+
+  -- Snippets
+  {
+    "L3MON4D3/LuaSnip",
+    version = "v2.*",
+    build = "make install_jsregexp",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+  },
 }
