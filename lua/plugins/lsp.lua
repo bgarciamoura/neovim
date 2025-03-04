b96603543915349d7879133de325a995c62c7525
Bruno Garcia Moura <itsme@bgarciamoura.com>
2025-03-04 17:37:12 -0300

    fix: Removed the lamps on trouble hints

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,25 +1,43 @@
 return {
-  -- Plugin principal do LSP
-  { "neovim/nvim-lspconfig" },
+	-- Plugin principal do LSP
+	{ "neovim/nvim-lspconfig" },
 
-  -- Mason: Gerenciador de LSPs
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-  },
+	-- Mason: Gerenciador de LSPs
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
 
-  -- Mason-LSPConfig: Integra Mason com o LSP
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "ts_ls", "eslint", "html", "cssls", "jsonls", "lua_ls" },
-      })
-      require("mason-tool-installer").setup({
-        ensure_installed = { "eslint_d", "prettier", "stylelint", "jsonlint", "stylua" },
-      })
-    end
-  }
+	-- Mason-LSPConfig: Integra Mason com o LSP
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "ts_ls", "html", "cssls", "jsonls", "lua_ls" },
+			})
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					"prettier",
+					"stylelint",
+					"jsonlint",
+					"stylua",
+					"biome", -- Ferramenta para linting mais rápida que o eslint
+					"prettierd", -- Formatação com Prettier mais rápida
+					"stylua", -- Formatação para Lua
+					"shfmt", -- Formatação para Shell Scripts
+					"luacheck", -- Linting para Lua
+					"htmlhint", -- Linting para HTML
+					"markdownlint", -- Linting para Markdown
+					"hadolint", -- Linting para Dockerfile
+					"yamllint", -- Linting para yaml
+					"taplo", -- Linting para toml
+					"xmllint", -- Linting para xml
+					"prisma-language-server", -- Linting para prisma
+					"dotenv-linter", -- Linting para env
+				},
+			})
+		end,
+	},
 }
