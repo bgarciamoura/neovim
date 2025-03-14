ef41bf26d1d4576f1f38661a0fc70ff674dfcf61
Bruno Garcia Moura <itsme@bgarciamoura.com>
2025-03-14 14:18:56 -0300

    feat: Remapped some keys and included DAP for debugging

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,9 +1,9 @@
 local function map(mode, target_keys, command, opts)
-  local options = { noremap = true, silent = true }
-  if opts then
-    options = vim.tbl_extend("force", options, opts)
-  end
-  vim.api.nvim_set_keymap(mode, target_keys, command, options)
+	local options = { noremap = true, silent = true }
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
+	vim.api.nvim_set_keymap(mode, target_keys, command, options)
 end
 
 map("n", "<C-a>", ":keepjumps normal! ggVG<CR>", { desc = "Select all text using control+a" })
@@ -11,10 +11,10 @@ map("n", "<A-k>", "<CMD>resize +10<CR>", { desc = "Increase window size to up" }
 map("n", "<A-j>", "<CMD>resize  -10<CR>", { desc = "Increase window size to down" })
 map("n", "<A-h>", "<CMD>vertical resize +10<CR>", { desc = "Increase window size to left" })
 map("n", "<A-l>", "<CMD>vertical resize -10<CR>", { desc = "Increase window size to right" })
-map("n", "<A-down>", "<C-w>j", { desc = "Move the cursor to the bottom window" })
-map("n", "<A-left>", "<C-w>h", { desc = "Move the cursor to the left window" })
-map("n", "<A-right>", "<C-w>l", { desc = "Move the cursor to the right window" })
-map("n", "<A-up>", "<C-w>k", { desc = "Move the cursor to the top window" })
+-- map("n", "<C-J>", "<C-w>j", { desc = "Move the cursor to the bottom window" })
+-- map("n", "<C-H>", "<C-w>h", { desc = "Move the cursor to the left window" })
+-- map("n", "<C-L>", "<C-w>l", { desc = "Move the cursor to the right window" })
+-- map("n", "<C-K>", "<C-w>k", { desc = "Move the cursor to the top window" })
 map("n", "<leader>cc", ':keepjumps normal! ggVG "*yG<CR>', { desc = "Copy the entire file to system clipboard" })
 map("n", "<leader>sv", ":vsplit<CR>", { desc = "Split the screen verticaly" })
 map("n", "<leader>sh", ":split<CR>", { desc = "Split the screen horizontaly" })
@@ -53,9 +53,9 @@ map("n", "<leader>gs", "<cmd>Gitsigns stage_hunk<CR>", { desc = "Adicionar alter
 map("n", "<leader>gr", "<cmd>Gitsigns reset_hunk<CR>", { desc = "Reverter alteração" })
 
 -- Persistence
-map("n", "<leader>ss", [[<cmd>lua require("persistence").load()<CR>]], { noremap = true, silent = true })                -- Restaurar última sessão
+map("n", "<leader>ss", [[<cmd>lua require("persistence").load()<CR>]], { noremap = true, silent = true }) -- Restaurar última sessão
 map("n", "<leader>ql", [[<cmd>lua require("persistence").load({ last = true })<CR>]], { noremap = true, silent = true }) -- Restaurar sessão mais recente
-map("n", "<leader>sd", [[<cmd>lua require("persistence").stop()<CR>]], { noremap = true, silent = true })                -- Desativar salvamento de sessão na sessão atual
+map("n", "<leader>sd", [[<cmd>lua require("persistence").stop()<CR>]], { noremap = true, silent = true }) -- Desativar salvamento de sessão na sessão atual
 
 -- Navegar entre buffers
 map("n", "<S-l>", ":bnext<CR>", { desc = "Próximo buffer" })
@@ -80,7 +80,7 @@ map("n", "<leader>o", "o<Esc>", { desc = "Nova linha abaixo" })
 map("n", "<leader>O", "O<Esc>", { desc = "Nova linha acima" })
 
 -- Salvar e fechar rapidamente
-map("n", "<leader>w", ":w<CR>", { desc = "Salvar arquivo" })
+map("n", "<C-s>", ":w<CR>", { desc = "Salvar arquivo" })
 map("n", "<leader>x", ":x<CR>", { desc = "Salvar e sair" })
 map("n", "<leader>Q", ":q!<CR>", { desc = "Sair sem salvar" })
 
@@ -101,7 +101,6 @@ map("v", "p", '"_dP', { desc = "Substituir texto sem afetar clipboard" })
 map("n", "<leader>t", ":split | terminal<CR>", { desc = "Abrir terminal no split" })
 map("n", "<leader>vt", ":vsplit | terminal<CR>", { desc = "Abrir terminal em vertical" })
 
-
 -- Selecionar e substituir
 map("n", "<leader>sr", ":%s//g<Left><Left>", { desc = "Selecionar e substituir no arquivo" })
 map("v", "<leader>sr", "y:%s/<C-r>0//g<Left><Left>", { desc = "Selecionar e substituir na seleção" })
@@ -110,15 +109,31 @@ map("n", "<leader>sw", ":Spectre<CR>", { desc = "Selecionar e substituir no work
 -- Neotest
 map("n", "<leader>js", "<cmd>lua require('neotest').run.stop()<cr>", { desc = "Parar execução de testes" })
 map("n", "<leader>jr", "<cmd>lua require('neotest').run.run()<cr>", { desc = "Executar testes" })
-map("n", "<leader>jf", "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>",
-  { desc = "Executar testes do arquivo" })
+map(
+	"n",
+	"<leader>jf",
+	"<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>",
+	{ desc = "Executar testes do arquivo" }
+)
 map("n", "<leader>jo", "<cmd>lua require('neotest').output.open()<cr>", { desc = "Abrir saída de testes" })
-map("n", "<leader>ju", "<cmd>lua require('neotest').summary.toggle()<cr>",
-  { desc = "Alternar visibilidade do resumo de testes" })
-map("n", "<leader>jp", "<cmd>lua require('neotest').jump.prev({status = 'failed'})<cr>",
-  { desc = "Ir para o próximo teste falho" })
-map("n", "<leader>jn", "<cmd>lua require('neotest').jump.next({status = 'failed'})<cr>",
-  { desc = "Ir para o teste anterior falho" })
+map(
+	"n",
+	"<leader>ju",
+	"<cmd>lua require('neotest').summary.toggle()<cr>",
+	{ desc = "Alternar visibilidade do resumo de testes" }
+)
+map(
+	"n",
+	"<leader>jp",
+	"<cmd>lua require('neotest').jump.prev({status = 'failed'})<cr>",
+	{ desc = "Ir para o próximo teste falho" }
+)
+map(
+	"n",
+	"<leader>jn",
+	"<cmd>lua require('neotest').jump.next({status = 'failed'})<cr>",
+	{ desc = "Ir para o teste anterior falho" }
+)
 
 -- Selecionar uma palavra para a esquerda do cursor
 map("n", "gw", "viw", { desc = "Selecionar palavra para a esquerda" })
@@ -127,7 +142,7 @@ map("n", "gw", "viw", { desc = "Selecionar palavra para a esquerda" })
 map("n", "ge", "vaw", { desc = "Selecionar palavra para a direita" })
 
 -- Selecionar texto entre aspas
-map("n", "gq", "vi\"", { desc = "Selecionar texto entre aspas" })
+map("n", "gq", 'vi"', { desc = "Selecionar texto entre aspas" })
 
 -- Selecionar texto entre parênteses
 map("n", "gQ", "vi)", { desc = "Selecionar texto entre parênteses" })
@@ -137,3 +152,7 @@ map("n", "g[", "vi[", { desc = "Selecionar texto entre colchetes" })
 
 -- Selecionar texto entre chaves
 map("n", "g{", "vi{", { desc = "Selecionar texto entre chaves" })
+
+-- Incrementar e decrementar números
+map("n", "<C-A>", "<C-a>", { desc = "Incrementar número" })
+map("n", "<C-x>", "<C-x>", { desc = "Decrementar número" })
