d7b013b12d515e52bd42bea8009ede6edbccf4db
Bruno Garcia Moura <itsme@bgarciamoura.com>
2025-03-14 16:12:08 -0300

    feat: Removed the lightbulb from lspsaga

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -1,10 +1,14 @@
 return {
-  'nvimdev/lspsaga.nvim',
-  config = function()
-    require('lspsaga').setup({})
-  end,
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter',     -- optional
-    'nvim-tree/nvim-web-devicons',         -- optional
-  }
+	"nvimdev/lspsaga.nvim",
+	config = function()
+		require("lspsaga").setup({
+			lightbulb = {
+				enabled = false,
+			},
+		})
+	end,
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter", -- optional
+		"nvim-tree/nvim-web-devicons", -- optional
+	},
 }
