29cdd8771af7ee3305b51bf2cbf609e0b3918f6c
Bruno Garcia Moura <itsme@bgarciamoura.com>
2025-03-14 15:56:57 -0300

    feat: Included some new configs for DAP

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -13,8 +13,32 @@ return {
 			local dap_virtual_text = require("nvim-dap-virtual-text")
 
 			-- Configuração do UI para DAP
-			dapui.setup()
-			dap_virtual_text.setup()
+			dapui.setup({
+				layouts = {
+					{
+						elements = {
+							{ id = "scopes", size = 0.25 },
+							{ id = "breakpoints", size = 0.25 },
+							{ id = "stacks", size = 0.25 },
+							{ id = "watches", size = 0.25 },
+						},
+						position = "left",
+						size = 40,
+					},
+					{
+						elements = {
+							{ id = "repl", size = 0.5 },
+							{ id = "console", size = 0.5 },
+						},
+						position = "bottom",
+						size = 10,
+					},
+				},
+			})
+			dap_virtual_text.setup({
+				enabled = true,
+				commented = true,
+			})
 
 			-- Integrar com eventos do dap
 			dap.listeners.after.event_initialized["dapui_config"] = function()
