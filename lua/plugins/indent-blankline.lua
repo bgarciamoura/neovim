f9bd4a7d141824ccf3a9e61b06afea94117e71a7
Bruno Garcia Moura <itsme@bgarciamoura.com>
2025-04-11 14:40:37 -0300

    fix: Made some changes to Indent-Blankline Highlight group

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,12 +1,13 @@
-vim.api.nvim_set_hl(0, "IndentBlankline", { fg = "#313131", nocombine = true })
-
 return {
 	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
 	config = function()
+		vim.cmd([[highlight IblIndent guifg=#313131 gui=nocombine]])
+
 		require("ibl").setup({
 			indent = {
 				char = "▏", -- Pode ser alterado para "⦙", "┆", "┊"
-				highlight = "IndentBlankline", -- Nome do destaque que vamos personalizar
+				highlight = "IblIndent", -- Nome do destaque que vamos personalizar
 			},
 		})
 	end,
