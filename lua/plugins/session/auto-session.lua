80836f87f3427d8a8b04a25bf4980c3a7b9cbe37
Bruno Garcia Moura <itsme@bgarciamoura.com>
2026-03-31 22:45:55 -0300

    fix(session): disable auto-restore to prevent stale splits

--- lua/plugins/session/auto-session.lua
+++ lua/plugins/session/auto-session.lua
@@ -2,7 +2,7 @@ return {
   "rmagatti/auto-session",
   lazy = false,
   opts = {
-    auto_restore = true,
+    auto_restore = false,
     auto_save = true,
     auto_create = true,
     suppressed_dirs = { "~/", "~/Downloads", "/" },
