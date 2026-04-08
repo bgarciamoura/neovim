6585bf16d460226fe1f2ad774ad9b8fa354d99be
Bruno Garcia Moura <itsme@bgarciamoura.com>
2026-04-08 17:29:50 -0300

    feat: register new plugins in vim.pack and expand mason ensure_installed
    Add ~28 plugins: ts-install, mini.ai, oasis, lualine, fidget, telescope,
    gitsigns, conform, nvim-lint, autopairs, surround, todo-comments,
    colorizer, SchemaStore, nvim-dap stack, neotest stack, toggleterm,
    flutter-tools. Configure mason to auto-install formatters (prettierd,
    black, stylua), linters (eslint_d, ruff, luacheck, markdownlint,
    hadolint), and DAP adapters (js-debug-adapter, debugpy).

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -6,13 +6,65 @@ vim.pack.add({
   'https://github.com/mason-org/mason-lspconfig.nvim',
   'https://github.com/neovim/nvim-lspconfig',
 
+  -- Treesitter parser installer (lightweight, no highlight/fold management)
+  'https://github.com/lewis6991/ts-install.nvim',
+
+  -- Textobjects via treesitter (no nvim-treesitter dependency)
+  'https://github.com/echasnovski/mini.ai',
+
   -- Keymap visibility
   'https://github.com/echasnovski/mini.clue',
 
+  -- Colorscheme
+  'https://github.com/uhs-robert/oasis.nvim',
+
+  -- Statusline
+  'https://github.com/nvim-lualine/lualine.nvim',
+
+  -- LSP progress
+  'https://github.com/j-hui/fidget.nvim',
+
+  -- Fuzzy finder
+  'https://github.com/nvim-telescope/telescope.nvim',
+  'https://github.com/nvim-telescope/telescope-fzf-native.nvim',
+  'https://github.com/nvim-telescope/telescope-ui-select.nvim',
+
+  -- Git
+  'https://github.com/lewis6991/gitsigns.nvim',
+
+  -- Formatting & linting
+  'https://github.com/stevearc/conform.nvim',
+  'https://github.com/mfussenegger/nvim-lint',
+
+  -- Editor
+  'https://github.com/windwp/nvim-autopairs',
+  'https://github.com/kylechui/nvim-surround',
+  'https://github.com/folke/todo-comments.nvim',
+  'https://github.com/norcalli/nvim-colorizer.lua',
+
+  -- JSON/YAML schema validation
+  'https://github.com/b0o/SchemaStore.nvim',
+
+  -- Debug
+  'https://github.com/mfussenegger/nvim-dap',
+  'https://github.com/rcarriga/nvim-dap-ui',
+  'https://github.com/theHamsta/nvim-dap-virtual-text',
+
+  -- Testing
+  'https://github.com/nvim-neotest/neotest',
+  'https://github.com/nvim-neotest/neotest-jest',
+  'https://github.com/marilari88/neotest-vitest',
+  'https://github.com/nvim-neotest/neotest-python',
+  'https://github.com/sidlatau/neotest-dart',
+
+  -- Terminal
+  'https://github.com/akinsho/toggleterm.nvim',
+
+  -- Flutter
+  'https://github.com/akinsho/flutter-tools.nvim',
+
   -- File explorer
   'https://github.com/nvim-neo-tree/neo-tree.nvim',
-  'https://github.com/nvim-lua/plenary.nvim',
-  'https://github.com/MunifTanjim/nui.nvim',
 
   -- Icons (material style)
   'https://github.com/nvim-tree/nvim-web-devicons',
@@ -21,10 +73,51 @@ vim.pack.add({
   -- Jupyter notebooks
   'https://github.com/benlubas/molten-nvim',
   'https://github.com/3rd/image.nvim',
+
+  -- Shared dependencies
+  'https://github.com/nvim-lua/plenary.nvim',
+  'https://github.com/MunifTanjim/nui.nvim',
+  'https://github.com/nvim-neotest/nvim-nio',
 }, { confirm = false })
 
 -- Mason setup (must come before mason-lspconfig)
-require('mason').setup()
+require('mason').setup({
+  ui = {
+    border = 'rounded',
+    icons = {
+      package_installed = '✓',
+      package_pending = '➜',
+      package_uninstalled = '✗',
+    },
+  },
+})
+
+-- Auto-install tools via mason-registry
+local registry = require('mason-registry')
+local ensure_installed = {
+  -- Formatters
+  'prettierd',
+  'black',
+  'stylua',
+  -- Linters
+  'eslint_d',
+  'ruff',
+  'luacheck',
+  'markdownlint',
+  'hadolint',
+  -- DAP adapters
+  'js-debug-adapter',
+  'debugpy',
+}
+
+registry.refresh(function()
+  for _, pkg_name in ipairs(ensure_installed) do
+    local ok, pkg = pcall(registry.get_package, pkg_name)
+    if ok and not pkg:is_installed() then
+      pkg:install()
+    end
+  end
+end)
 
 require('mason-lspconfig').setup({
   ensure_installed = {
@@ -32,15 +125,27 @@ require('mason-lspconfig').setup({
     'pyright',
     'ruff',
     'lua_ls',
+    'jsonls',
+    'yamlls',
+    'html',
+    'cssls',
+    'taplo',
+    'marksman',
+    'dockerls',
+    'docker_compose_language_service',
   },
-  -- Only auto-enable the servers we explicitly configured
-  -- Prevents mason from enabling all installed servers (bashls, eslint, tailwindcss, etc.)
   automatic_enable = {
     'ts_ls',
     'pyright',
     'ruff',
     'lua_ls',
+    'jsonls',
+    'yamlls',
+    'html',
+    'cssls',
+    'taplo',
+    'marksman',
+    'dockerls',
+    'docker_compose_language_service',
   },
 })
--- dartls is not managed by mason (comes with Flutter SDK)
-vim.lsp.enable('dartls')
