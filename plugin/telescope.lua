e723cc53643dd67a26522338a4f7226797ab20de
Bruno Garcia Moura <itsme@bgarciamoura.com>
2026-04-08 17:30:26 -0300

    feat: add telescope fuzzy finder with fzf and ui-select
    Configure telescope with top prompt, fzf-native sorter, ui-select integration,
    custom mappings (C-j/C-k navigation, C-q quickfix), and file ignore patterns.

---
+++ plugin/telescope.lua
@@ -0,0 +1,56 @@
+-- Fuzzy finder
+
+local telescope = require('telescope')
+local actions = require('telescope.actions')
+local themes = require('telescope.themes')
+
+telescope.setup({
+  defaults = {
+    prompt_prefix = '   ',
+    selection_caret = '  ',
+    entry_prefix = '  ',
+    sorting_strategy = 'ascending',
+    layout_strategy = 'horizontal',
+    layout_config = {
+      horizontal = {
+        prompt_position = 'top',
+        preview_width = 0.55,
+        width = 0.87,
+        height = 0.80,
+      },
+    },
+    file_ignore_patterns = {
+      'node_modules',
+      '.git/',
+      '__pycache__',
+      '.venv',
+      '%.lock',
+    },
+    mappings = {
+      i = {
+        ['<C-j>'] = actions.move_selection_next,
+        ['<C-k>'] = actions.move_selection_previous,
+        ['<C-q>'] = actions.send_to_qflist + actions.open_qflist,
+        ['<Esc>'] = actions.close,
+      },
+    },
+  },
+  pickers = {
+    find_files = { hidden = true },
+    buffers = { theme = 'dropdown', previewer = false },
+  },
+  extensions = {
+    fzf = {
+      fuzzy = true,
+      override_generic_sorter = true,
+      override_file_sorter = true,
+      case_mode = 'smart_case',
+    },
+    ['ui-select'] = {
+      themes.get_dropdown(),
+    },
+  },
+})
+
+pcall(telescope.load_extension, 'fzf')
+pcall(telescope.load_extension, 'ui-select')
