db04201061470d5cd932947d4c72978c42b77cc7
Bruno Garcia Moura <itsme@bgarciamoura.com>
2026-04-10 11:18:15 -0300

    fix: silence vim.tbl_flatten deprecation warning from plugins

--- init.lua
+++ init.lua
@@ -1,5 +1,8 @@
 vim.g._start_time = vim.loop.hrtime()
 
+-- Silence vim.tbl_flatten deprecation from plugins (redirect to new API)
+vim.tbl_flatten = function(t) return vim.iter(t):flatten():totable() end
+
 require("config.options")
 require("config.plugins")
 require("config.autocmds")
